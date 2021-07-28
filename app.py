7816c8d1ad8be64b92f398bbaf4df7363335abde
Amenable <duswo0624@naver.com>
2021-07-28 17:19:34 +0900

    practicing the req, res

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 # Flask 앱 서버 인스터스
 app = Flask(__name__)
@@ -7,8 +7,28 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "<h1>hello<h1>"
+def hello():
+    # get을 통한 전달받은 데이터를 확인
+    return render_template('hello.html')
+
+
+@app.route('/applyphoto')
+def photo_apply():
+    location = request.args.get("location")
+    cleaness = request.args.get("clean")
+    built_in = request.args.get("built")
+    print(location, cleaness, built_in)
+    # return render_template('apply.html')
+
+
+@app.route('/apply')
+def apply():
+    return render_template('apply.html')
+
+
+@app.route('/list')
+def list():
+    return render_template('list.html')
 
 
 # 메인 테스트
